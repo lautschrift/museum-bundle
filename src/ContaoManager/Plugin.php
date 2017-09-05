714c337c4f92e0c89e5517d2d2b5c3f87b531bbc
lautschrift <steffen@lautschrift.com>
2017-09-05 17:13:47 +0200

    Plugin added

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+namespace Lautschrift\MuseumBundle;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+	public function getBundles(ParserInterface $parser)
+	{
+		return [
+				BundleConfig::create(MuseumBundle::class) 
+				->setLoadAfter([ContaoCoreBundle::class])
+				->setReplace(['museum'])
+		]; }
+}
