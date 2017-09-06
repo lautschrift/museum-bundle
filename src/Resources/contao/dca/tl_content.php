48e8c6f9373dc1c5a622737ff87df5d8c3146c5a
lautschrift <steffen@lautschrift.com>
2017-09-06 16:42:23 +0200

    wip

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -5,19 +5,33 @@
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['museum'] = '
 	{type_legend},type,headline;
-	{museum_legend},museumSet;
+	{museum_legend},museum_name, museum_street, museum_nr, museum_plz;
 	{template_legend:hide},customTpl;
 	{protected_legend:hide},protected;
 	{expert_legend:hide},guests,cssID;
 	{invisible_legend:hide},invisible,start,stop
 ';
 
-$GLOBALS['TL_DCA']['tl_content']['fields'][museumSet] = array(
-
-		'name' => array(
-				'label' => array('Name', 'Name des Museums'),
-				'eval' => array('tl_class' ),
-				'inputType' => 'text',
-		)
-	
+/***
+ * Fields
+ */
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_name] = array(
+		'label' => array('Name', 'Name des Museums'),
+		'eval' => array('tl_class' ),
+		'inputType' => 'text',	
+);
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_street] = array(
+		'label' => array('Straße', 'Straße'),
+        'eval' => array('tl_class' => 'w50'),
+        'inputType' => 'text',
+);
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_nr] = array(
+		'label' => array('Hausnumer', 'Hausnummer'),
+        'eval' => array('tl_class' => 'w50'),
+        'inputType' => 'text',
+);
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_plz] = array(
+		  'label' => array('Postleitzahl', 'Postleitzahl'),
+          'eval' => array('tl_class' => 'w50'),
+          'inputType' => 'text',
 );
