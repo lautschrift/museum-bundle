44a664125d9827eacd3034061ae74c70d524e230
lautschrift <steffen@lautschrift.com>
2017-09-06 16:20:39 +0200

    wip

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -18,52 +18,6 @@ $GLOBALS['TL_DCA']['tl_content'][museumSet] = array(
 				'label' => array('Name', 'Name des Museums'),
 				'eval' => array('tl_class' ),
 				'inputType' => 'text',
-		),
-
-		'str' => array(
-				'label' => array('Straße', 'Straße'),
-				'eval' => array('tl_class' => 'w50'),
-				'inputType' => 'text',
-		),
-		'nr' => array(
-				'label' => array('Hausnumer', 'Hausnummer'),
-				'eval' => array('tl_class' => 'w50'),
-				'inputType' => 'text',
-		),
-		'plz' => array(
-				'label' => array('Postleitzahl', 'Postleitzahl'),
-				'eval' => array('tl_class' => 'w50'),
-				'inputType' => 'text',
-		),
-		'ort' => array(
-				'label' => array('Ort', 'Ort'),
-				'eval' => array('tl_class' => 'w50'),
-				'inputType' => 'text',
-		),
-		'land' => array(
-				'label' => array('Land', ''),
-				'eval' => array('tl_class' => 'clr'),
-				'options' => array('Deutschland', 'Österreich', 'Schweiz', 'Italien', 'Slowenien'),
-				'inputType' => 'select',
-		),
-		'email' => array(
-				'label' => array('E-Mail', 'E-Mail Adresse'),
-				'eval' => array('tl_class' => 'w50'),
-				'inputType' => 'text',
-		),
-		'website' => array(
-				'label' => array('Website', 'Website Adresse'),
-				'eval' => array('tl_class' => 'w50'),
-				'inputType' => 'url',
-		),
-		'description' => array(
-				'label' => array('Infotext', 'Beschreibung und weiterführende Informationen zum Museum'),
-				'inputType' => 'group',
-		),
-		'museuminfo' => array(
-				'label' => array('Info-Text', 'Beschreibung und weiterführende Informationen zum Museum'),
-				'eval' => array('rte' => 'tinyMCE'),
-				'inputType' => 'textarea',
 		)
 	
 );
