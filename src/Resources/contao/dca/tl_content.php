346ce635cbd2f3b4f902535e1c40913e58d2f175
lautschrift <steffen@lautschrift.com>
2017-09-07 08:56:13 +0200

    wip

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -4,8 +4,9 @@
  */
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['museum'] = '
-	{type_legend},type,headline;
-	{museum_legend},museum_name, museum_street, museum_nr, museum_plz;
+	{type_legend},type,headline ,image;
+	{museum_legend},museum_name, museum_street, museum_nr, museum_plz, museum_ort, museum_land, museum_email, museum_website;
+	{museumtext_legend},text;
 	{template_legend:hide},customTpl;
 	{protected_legend:hide},protected;
 	{expert_legend:hide},guests,cssID;
@@ -39,3 +40,29 @@ $GLOBALS['TL_DCA']['tl_content']['fields'][museum_plz] = array(
         'inputType'	=> 'text',
 		'sql'       => "varchar(255) NOT NULL default ''"
 );
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_ort] = array(
+		'label' 	=> array('Postleitzahl', 'Postleitzahl'),
+		'eval' 		=> array('tl_class' => 'w50'),
+		'inputType' => 'text',
+		'sql'       => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_land] = array(
+		'label' 	=> array('Land', ''),
+        'eval' 		=> array('tl_class' => 'clr'),
+		'options' 	=> array('Deutschland', 'Österreich', 'Schweiz', 'Italien', 'Slowenien'),
+        'inputType' => 'select',
+		'sql'       => "int(10) unsigned NOT NULL default '0'"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_email] = array(
+		'label' 	=> array('E-Mail', 'E-Mail Adresse'),
+		'eval' 		=> array('tl_class' => 'w50'),
+		'inputType' => 'url',
+		'sql'       => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_website] = array(
+		'label' 	=> array('Website', 'Website Adresse'),
+		'eval' 		=> array('tl_class' => 'w50'),
+		'inputType' => 'url',
+		'sql'       => "varchar(255) NOT NULL default ''"
+);
+
