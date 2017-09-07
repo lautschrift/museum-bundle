14858a5c63fcefb61ff35003d29b85a08b08cf42
lautschrift <steffen@lautschrift.com>
2017-09-07 09:39:46 +0200

    wip

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -5,7 +5,7 @@
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['museum'] = '
 	{type_legend},type,headline;
-	{museum_legend},museum_name, museum_street, museum_nr, museum_plz, museum_ort, museum_land, museum_email, museum_website;
+	{museum_legend},museum_name, museum_street, museum_nr, museum_plz, museum_ort, museum_land, museum_email, museum_website, museum_geox, museum_geoy;
 	{museumtext_legend},text;
 	{source_legend},singleSRC,size,imagemargin,fullsize,overwriteMeta;
 	{c4g_map_legend},c4g_map_id,c4g_map_default_mapservice,c4g_map_width,c4g_map_height,c4g_map_zoom;
@@ -67,9 +67,25 @@ $GLOBALS['TL_DCA']['tl_content']['fields'][museum_website] = array(
 		'inputType' => 'url',
 		'sql'       => "varchar(255) NOT NULL default ''"
 );
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_geox] = array(
+		'label' =>array('Karte geoX', ''),
+					
+					'inputType'               => 'c4g_text',
+					'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
+					'save_callback'           => array(array('tl_calendar_events_c4g_maps','setLocLon')),
+					'wizard'                  => array(array('GeoPicker', 'getPickerLink')),
+					'sql'                     => "varchar(20) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_content']['fields'][museum_geoy] = array(
+		'label' =>array('Karte geoY', ''),
+			
+		'inputType'               => 'c4g_text',
+		'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
+		'save_callback'           => array(array('tl_calendar_events_c4g_maps','setLocLat ')),
+		'wizard'                  => array(array('GeoPicker', 'getPickerLink')),
+		'sql'                     => "varchar(20) NOT NULL default ''"
+);
 
-
-// con4gis
 $GLOBALS['TL_DCA']['tl_content']['fields']['c4g_map_id'] = array
 (
 		'label'                   => &$GLOBALS['TL_LANG']['tl_content']['c4g_map_id'],
