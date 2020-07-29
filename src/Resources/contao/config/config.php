758074d694487440ae257520e494514ed62defff
lautschrift <steffen@lautschrift.com>
2020-07-29 18:01:25 +0200

    wip

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Back end modules
+*/
+
+/*
+array_insert($GLOBALS['BE_MOD']['content'], 1, array
+(
+	'Museum' => array
+	(
+		'tables'	=> array('tl_museum'),
+
+	)
+));
+*/
+/**
+ * Content elements
+*/
+/*
+array_insert($GLOBALS['TL_CTE']['Museum'] ,1, array(
+		'museum' => 'Lautschrift\\Museum-Bundle\\ContentMuseum'
+));
+
+*/
+
+/*
+
+$GLOBALS['TL_CTE']['Museum'] = array
+(
+	'museum' => 'MuseumBundle\Resources\ContentMuseum',
+
+);
+
+
+array_insert($GLOBALS['TL_CTE']['Museum'] ,1, array(
+		'museum' => 'Lautschrift\\MuseumBundle\\Resources\\contao\\classes\\ContentMuseum'
+));
+*/
+
+$GLOBALS['TL_CTE']['Museum'] = array(
+	'museum' => 'Lautschrift\\MuseumBundle\\Resources\\contao\\classes\\ContentVitrine'
+);
+
+//$GLOBALS['TL_CTE']['Museum']['Museum'] = '\\lautschrift\\museum-bundle\\src\\Resources\\contao\\classes\\ContentMuseum';
+$GLOBALS['TL_LANG']['CTE']['Museum'] = array('Museum oder Fundstelle','Einbinden einer Museum-Karte über OpenLayers.');
