ffbd2750d4a35430b322a650412131975f60858e
lautschrift <steffen@lautschrift.com>
2020-07-30 20:15:16 +0200

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -99,7 +99,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['museum_geox'] = array(
 		'label' 				  => array('Karte geoX', ''),
 		'inputType'               => 'c4g_text',
 		'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
-        'save_callback'           => [['tl_content_c4g_maps', 'setLocLon']],
+        //'save_callback'           => [['tl_content_c4g_maps', 'setLocLon']],
         'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoPicker', 'getPickerLink']],
 		'sql'                     => "varchar(20) NOT NULL default ''"
 );
@@ -107,7 +107,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['museum_geoy'] = array(
 		'label' 				  => array('Karte geoY', ''),
 		'inputType'               => 'c4g_text',
 		'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
-        'save_callback'           => [['tl_content_c4g_maps', 'setLocLat']],
+        //'save_callback'           => [['tl_content_c4g_maps', 'setLocLat']],
         'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoPicker', 'getPickerLink']],
 		'sql'                     => "varchar(20) NOT NULL default ''"
 );
