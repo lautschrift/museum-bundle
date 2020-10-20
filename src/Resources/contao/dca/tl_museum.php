9cb80b2a9e347916232d667f06d5e45d6e714def
lautschrift <steffen@lautschrift.com>
2020-10-20 15:31:01 +0200

    Update tl_museum.php

--- src/Resources/contao/dca/tl_museum.php
+++ src/Resources/contao/dca/tl_museum.php
@@ -2,9 +2,100 @@
 /**
  * Table tl_museum
  */
-$GLOBALS['TL_DCA']['tl_museum'] = array
+$GLOBALS['TL_DCA']['tl_museum'] = [
+	// Config
+	'config' => [
+
+	'dataContainer'               => 'Table',
+	//'ptable'                      => 'tl_content',
+	'ctable'                      => ['tl_content'],
+	'enableVersioning'            => true,
+	'sql' => array
+		(
+		'keys' => array
+			(
+			'id' => 'primary'
+			)
+		)
+	],
+	//List
+	'list' => [
+        'sorting' => [
+            'mode' => 1,
+            'fields' => ['name'],
+            'flag' => 1,
+            'panelLayout' => 'search,limit'
+        ],
+        'label' => [
+            'fields' => ['name'],
+            'format' => '%s',
+        ],
+        'operations' => [
+            'edit' => [
+                'href' => 'table=tl_museum',
+                'icon' => 'edit.svg',
+            ],
+            'editheader' => [
+                'href' => 'act=edit',
+                'icon' => 'header.svg',
+            ],
+            'delete' => [
+                'href' => 'act=delete',
+                'icon' => 'delete.svg',
+            ],
+            'show' => [
+                'href' => 'act=show',
+                'icon' => 'show.svg'
+            ],
+        ],
+    ],
+		// Fields
+     'fields' => [
+        'id' => [
+            'sql' => ['type' => 'integer', 'unsigned' => true, 'autoincrement' => true]
+        ],
+        'tstamp' => [
+            'sql' => ['type' => 'integer', 'unsigned' => true, 'default' => 0]
+        ],
+				'name' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_museum']['name'],
+            'search' => true,
+            'inputType' => 'text',
+            'eval' => ['tl_class' => 'w50', 'maxlength' => 255, 'mandatory' => true],
+            'sql' => ['type' => 'string', 'length' => 255, 'default' => '']
+        ],
+				'geoX' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_site']['geoX'],
+            'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
+            'inputType'               => 'c4g_text',
+            'save_callback'           => [['tl_site_c4g_maps_site', 'setLocLon']],
+            'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoPicker', 'getPickerLink']],
+            'sql' => ['type' => 'string', 'length' => 20, 'default' => '']
+        ],
+        'geoY' => [
+            'label' => &$GLOBALS['TL_LANG']['tl_site']['geoY'],
+            'eval'                    => array('mandatory'=>false, 'maxlength'=>20, 'tl_class'=>'w50 wizard' ),
+            'inputType'               => 'c4g_text',
+            'save_callback'           => [['tl_site_c4g_maps_site', 'setLocLat']],
+            'wizard'                  => [['\con4gis\MapsBundle\Classes\GeoPicker', 'getPickerLink']],
+            'sql' => ['type' => 'string', 'length' => 20, 'default' => '']
+        ],
+			],
+
+			// Palettes
+     'palettes' => [
+         'default' => '{site_legend},name;'
+    ]
+];
+
+
+
+
+
+
+/*array
 (
-	'dataContainer'               => 'Table',	
+	'dataContainer'               => 'Table',
 	'switchToEdit'                => true,
 	'enableVersioning'            => true,
 	'sql' => array
@@ -16,8 +107,8 @@ $GLOBALS['TL_DCA']['tl_museum'] = array
 		)
 	)
 );
-
+*/
 class tl_museum extends Backend
 {
-	
+
 }
