c513ed73328aabf90151a4edad52e70b906be7e8
lautschrift <steffen@lautschrift.com>
2020-10-22 12:29:28 +0200

    Update tl_museum.php

--- src/Resources/contao/dca/tl_museum.php
+++ src/Resources/contao/dca/tl_museum.php
@@ -64,7 +64,7 @@ $GLOBALS['TL_DCA']['tl_museum'] = [
 						(
 							array('tl_museum', 'saveAlias'),
 						),
-						'options_callback' => array('tl_museum', 'getAlias');
+						'options_callback' 				=> array('tl_museum', 'getAlias'),
 						'sql'                     => "int(10) unsigned NOT NULL default 0",
 						'relation'                => array('type'=>'hasOne', 'load'=>'lazy', 'table'=>'tl_museum')
 				],
@@ -154,7 +154,7 @@ $GLOBALS['TL_DCA']['tl_museum'] = [
             'inputType' => 'fileTree',
             'eval' => [
                 'tl_class' => 'clr',
-                'mandatory' => true,
+                'mandatory' => false,
                 'fieldType' => 'radio',
                 'filesOnly' => true,
                 'extensions' => \Contao\Config::get('validImageTypes'),
