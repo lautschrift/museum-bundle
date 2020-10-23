d6c17e6a5e82ea3b8dbfae0aa8fd28c88dbcb81a
lautschrift <steffen@lautschrift.com>
2020-10-23 13:06:17 +0200

    Create tl_museum_details.php

---
+++ src/Resources/contao/languages/de/tl_museum_details.php
@@ -0,0 +1,12 @@
+$GLOBALS['TL_LANG']['tl_museum_details']['museum_legend'] = 'Informationen zum Museum';
+$GLOBALS['TL_LANG']['tl_museum_details']['speech_legend'] = 'Anzeigesprache';
+
+$GLOBALS['TL_LANG']['tl_museum_details']['speech'] = 'Anzeigesprache';
+$GLOBALS['TL_LANG']['tl_museum_details']['published'] = 'Veröffentlicht';
+
+$GLOBALS['TL_LANG']['tl_museum']['wert1'] = 'Deutschland';
+$GLOBALS['TL_LANG']['tl_museum']['wert2'] = 'Österreich';
+$GLOBALS['TL_LANG']['tl_museum']['wert3'] = 'Schweiz';
+$GLOBALS['TL_LANG']['tl_museum']['wert4'] = 'Italien';
+$GLOBALS['TL_LANG']['tl_museum']['wert5'] = 'Slowenien';
+$GLOBALS['TL_LANG']['tl_museum']['wert6'] = 'Frankreich';
