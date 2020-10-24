04bbbd0dafa476557f0f4cdedd6f1f3c8a7adf93
lautschrift <steffen@lautschrift.com>
2020-10-24 11:14:39 +0200

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -6,19 +6,17 @@ if (!defined('TL_ROOT')) die('You can not access this file directly!');
  * Palettes
  */
 $GLOBALS['TL_DCA']['tl_module']['palettes']['tl_museum']        =   '{title_legend},name;'.
-                                                                    '{test_legend}';
+                                                                    '{test_legend},speech';
 /***
  * Fields
  */
-$GLOBALS['TL_DCA']['tl_module']= [
-  'fields' => [
-    'speech' => [
-        'label' => &$GLOBALS['TL_LANG']['tl_site']['speech'],
-        'inputType' => 'select',
-        'eval' 		=> array('submitOnChange' => true, 'tl_class' => 'clr', 'mandatory' => true),
-        'options' 	=> ['DE', 'EN', 'FR', 'SLO', 'IT'],
-        'reference' => &$GLOBALS['TL_LANG']['site_details_speech'],
-        'sql' => ['type' => 'string', 'length' => 3, 'default' => 0]
-    ],
-  ]
-];
+$GLOBALS['TL_DCA']['tl_module']['fields']['speech'] = array (
+  'speech' => [
+      'label' => &$GLOBALS['TL_LANG']['tl_site']['speech'],
+      'inputType' => 'select',
+      'eval' 		=> array('submitOnChange' => true, 'tl_class' => 'clr', 'mandatory' => true),
+      'options' 	=> ['DE', 'EN', 'FR', 'SLO', 'IT'],
+      'reference' => &$GLOBALS['TL_LANG']['site_details_speech'],
+      'sql' => ['type' => 'string', 'length' => 3, 'default' => 0]
+  ],
+);
