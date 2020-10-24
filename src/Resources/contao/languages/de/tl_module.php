c77f8382adefd072475103a90c5c5e41edac1f1f
lautschrift <steffen@lautschrift.com>
2020-10-24 13:01:46 +0200

    Update tl_module.php

--- src/Resources/contao/languages/de/tl_module.php
+++ src/Resources/contao/languages/de/tl_module.php
@@ -1,2 +1,2 @@
-$GLOBALS['TL_LANG']['tl_module']['museumspeech_legend'] = 'Landeseinstellung'
+$GLOBALS['TL_LANG']['tl_module']['museumspeech_legend'] = 'Landeseinstellung';
 $GLOBALS['TL_LANG']['tl_module']['museum_speech'] = ['Landeszuordnung', 'Übersicht aller Museen des eingestellten Landes'];
