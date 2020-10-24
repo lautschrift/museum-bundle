922b7391c61842421b1a4f54f00c581ac7b932a9
lautschrift <steffen@lautschrift.com>
2020-10-24 09:58:09 +0200

    Update tl_module.php

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -10,12 +10,15 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['tl_museum']        =   '{title_lege
 /***
  * Fields
  */
-$GLOBALS['TL_DCA']['tl_module']['fields'] =
-'speech' => [
-    'label' => &$GLOBALS['TL_LANG']['tl_site']['speech'],
-    'inputType' => 'select',
-    'eval' 		=> array('submitOnChange' => true, 'tl_class' => 'clr', 'mandatory' => true),
-    'options' 	=> ['DE', 'EN', 'FR', 'SLO', 'IT'],
-    'reference' => &$GLOBALS['TL_LANG']['site_details_speech'],
-    'sql' => ['type' => 'string', 'length' => 3, 'default' => 0]
+$GLOBALS['TL_DCA']['tl_module']= [
+  'fields' => [
+    'speech' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_site']['speech'],
+        'inputType' => 'select',
+        'eval' 		=> array('submitOnChange' => true, 'tl_class' => 'clr', 'mandatory' => true),
+        'options' 	=> ['DE', 'EN', 'FR', 'SLO', 'IT'],
+        'reference' => &$GLOBALS['TL_LANG']['site_details_speech'],
+        'sql' => ['type' => 'string', 'length' => 3, 'default' => 0]
+    ],
+  ]
 ];
